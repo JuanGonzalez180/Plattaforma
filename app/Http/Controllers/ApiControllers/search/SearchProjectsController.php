935b3469cfc611ce6e47ead7065062f186dfb603
Juangon180 <juangon.t@gmail.com>
2021-06-11 16:20:16 -0500

    Remove Comments

--- app/Http/Controllers/ApiControllers/search/SearchProjectsController.php
+++ app/Http/Controllers/ApiControllers/search/SearchProjectsController.php
@@ -38,21 +38,6 @@ class SearchProjectsController extends ApiController
                                         ->orderBy('id', 'desc')
                                         ->get();
 
-            foreach( $projects as $key => $project ){
-                // $project->user['url'] = $project->user->image ? url( 'storage/' . $project->user->image->url ) : null;
-                // $project->company;
-                // $project->company->image;
-                /*unset($project->user['id']);
-                unset($project->user['username']);
-                unset($project->user['email']);
-                unset($project->user['email_verified_at']);
-                unset($project->user['verified']);
-                unset($project->user['validated']);
-                unset($project->user['admin']);
-                unset($project->user['card_brand']);
-                unset($project->user['card_last_four']);
-                unset($project->user['trial_ends_at']);*/
-            }
             return $this->showAllPaginate($projects);
         }
         return [];
