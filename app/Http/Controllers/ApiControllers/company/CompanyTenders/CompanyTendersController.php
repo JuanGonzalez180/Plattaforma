331a700ac222f0fd2474fd03d318f2d8c7094136
Cristian <cris10x@hotmial.com>
2021-06-29 17:47:14 -0500

    se hicieron ajustes

--- app/Http/Controllers/ApiControllers/company/CompanyTenders/CompanyTendersController.php
+++ app/Http/Controllers/ApiControllers/company/CompanyTenders/CompanyTendersController.php
@@ -118,10 +118,8 @@ class CompanyTendersController extends ApiController
         $tendersTransformer = new TendersTransformer();
 
         foreach($tender->tendersVersion as $key => $version) {
-            if($version->status == TendersVersions::LICITACION_PUBLISH)
-                continue;
-
-            unset($tender->tendersVersion[$key]);
+            if($version->status == TendersVersions::LICITACION_CREATED)
+                unset($tender->tendersVersion[$key]);
         };
         
         if ( $company_status == TendersCompanies::STATUS_PARTICIPATING || $company_status == TendersCompanies::STATUS_PROCESS ){
