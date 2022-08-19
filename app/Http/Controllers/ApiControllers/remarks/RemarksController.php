a43c862788e5d8055a6ba597eaed5435552d99d5
cristian_fajardo <cris10app@gmail.com>
2022-08-19 08:47:38 -0500

    nuevos cambios

--- app/Http/Controllers/ApiControllers/remarks/RemarksController.php
+++ app/Http/Controllers/ApiControllers/remarks/RemarksController.php
@@ -8,12 +8,14 @@ use App\Models\Remarks;
 use App\Models\Products;
 use App\Models\Projects;
 use App\Models\TendersCompanies;
+use App\Models\QuotesCompanies;
 use Illuminate\Http\Request;
 use App\Http\Controllers\ApiControllers\ApiController;
 
 class RemarksController extends ApiController
 {
-    public function validateUser(){
+    public function validateUser()
+    {
         try {
             $this->user = JWTAuth::parseToken()->authenticate();
         } catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
@@ -24,20 +26,20 @@ class RemarksController extends ApiController
     public function index(Request $request)
     {
         $user = $this->validateUser();
-        
-        if( $request->type === 'tenders' ){
+
+        if ($request->type === 'tenders') {
             $item = TendersCompanies::find($request->id);
-        }elseif( $request->type === 'projects' ){
+        } elseif ($request->type === 'projects') {
             $item = Projects::find($request->id);
-        }elseif( $request->type === 'products' ){
+        } elseif ($request->type === 'products') {
             $item = Products::find($request->id);
-        }elseif( $request->type === 'companies' ){
+        } elseif ($request->type === 'companies') {
             $item = Company::find($request->id);
         }
 
         foreach ($item->remarks as $key => $remark) {
-            if( $remark->user_id == $user->id ){
-                return $this->showOne($remark,200);
+            if ($remark->user_id == $user->id) {
+                return $this->showOne($remark, 200);
             }
         }
         return [];
@@ -47,48 +49,50 @@ class RemarksController extends ApiController
     {
         //
         $user = $this->validateUser();
-        if( $request->calification && $request->message ){
-            if( $request->type === 'tenders' ){
+        if ($request->calification && $request->message) {
+            if ($request->type === 'tenders') {
                 $item = TendersCompanies::find($request->id);
                 $companyId = ($user->userType() == 'oferta') ? $item->tender->company_id : $item->company_id;
-            }elseif( $request->type === 'products' ){
+            } elseif ($request->type === 'quotes') {
+                $item = QuotesCompanies::find($request->id);
+                $companyId = ($user->userType() == 'oferta') ? $item->quote->company_id : $item->company_id;
+            } elseif ($request->type === 'products') {
                 $item = Products::find($request->id);
                 $companyId = $item->company->id;
-            }elseif( $request->type === 'projects' ){
+            } elseif ($request->type === 'projects') {
                 $item = Projects::find($request->id);
                 $companyId = $item->company->id;
-            }elseif( $request->type === 'companies' ){
+            } elseif ($request->type === 'companies') {
                 $item = Company::find($request->id);
                 $companyId = $item->id;
             }
 
-            $findRemarksBoolean = false; 
+            $findRemarksBoolean = false;
             $findRemarks;
             foreach ($item->remarks as $key => $remark) {
-                if( $remark->user_id == $user->id ){
+                if ($remark->user_id == $user->id) {
                     $findRemarksBoolean = true;
                     $findRemarks = $remark;
                 }
             }
 
-            if( $findRemarksBoolean ){
+            if ($findRemarksBoolean) {
                 $findRemarks->delete();
             }
 
             $item->remarks()->create([
-                'user_id' => $user->id, 
-                'company_id' => $companyId, 
+                'user_id' => $user->id,
+                'company_id' => $companyId,
                 'calification'  => $request->calification,
                 'message'  => $request->message
             ]);
 
-            return $this->showOne($item,200);
-        }else{
-            $calificationError = [ 'calification' => 'Error, no se ha podido registrar la calificación' ];
-            return $this->errorResponse( $calificationError, 500 );
+            return $this->showOne($item, 200);
+        } else {
+            $calificationError = ['calification' => 'Error, no se ha podido registrar la calificación'];
+            return $this->errorResponse($calificationError, 500);
         }
 
         return [];
     }
-
 }
