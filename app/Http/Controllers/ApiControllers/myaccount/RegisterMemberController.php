b00400c1f907813003adc9a9e164fd1f21b9475c
Cristian Fajardo <cris10app@gmail.com>
2023-01-04 16:08:12 -0500

    se ha modificado la api

--- app/Http/Controllers/ApiControllers/myaccount/RegisterMemberController.php
+++ app/Http/Controllers/ApiControllers/myaccount/RegisterMemberController.php
@@ -39,6 +39,7 @@ class RegisterMemberController extends ApiController
                 $dataTeam->update( $dataMember );
             } else {
                 $newMember["already_registered"] = true;
+                $newMember["companyName"] = $newMember->companyName();
             }
 
             // $newMember->companyName = $newMember->company->name;
